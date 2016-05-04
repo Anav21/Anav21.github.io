4a997334761068b91b3b586d4db69f7a06061e6e
Anav21 <anav2121@live.com>
2016-05-04 12:37:24 -0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+$_SESSION['pagename'] = "Echo index";
+include "db/pushhits.php";
+?>
+<?php
+
+session_start();
+$_SESSION['pagename'] = "Echo index";
+include "db/pushhits.php";
+?>
+
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Echo- Adrian</title>
+
+<style>
+body {background-color:gray}
+h1   {color:blue}
+p    {color:blue}
+</style>
+
+</head>
+<body>
+
+<h1 style="text-align:center">Adrian's web page</h1>
+<p>thats it...</p>
+<img src="/home/images/chiwanahs.png" alt="Go Eagles!" style="width:128px;height;128px;">
+</body>
+</html> 
---
+++ info.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$_SESSION['pagename'] = "Echo index";
+include "db/pushhits.php";
+?>
